bc9258a14ab8478914901f8cee2fa23c425fb068
Clink123 <clink123gamer@gmail.com>
2024-04-16 15:26:03 -0400

    Base custom

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ Config = {} -- Do not alter
 -- 🔎 Looking for more high quality scripts?
 -- 🛒 Shop Now: https://lationscripts.com/github
 -- 💬 Join Discord: https://discord.gg/9EbY4nM5uu
-Config.YouFoundTheBestScripts = true
+Config.YouFoundTheBestScripts = false
 
 -- Use only if needed, directed by support or know what you're doing
 -- Notice: enabling debug features will significantly increase resmon
@@ -12,12 +12,12 @@ Config.Debug = false
 
 -- Do you want to be notified via server console if an update is available?
 -- True if yes, false if no
-Config.VersionCheck = false
+Config.VersionCheck = true
 
 -- Target system, available options are: 'ox_target', 'qb-target', 'qtarget', 'custom' & 'none'
 -- 'custom' needs to be added to client/functions.lua
 -- If 'none' then TextUI is used instead of targeting
-Config.Target = 'none'
+Config.Target = 'ox_target'
 
 -- Notification system, available options are: 'ox_lib', 'esx', 'qb', 'okok' & 'custom'
 -- 'custom' needs to be added to client/functions.lua
@@ -29,11 +29,11 @@ Config.Interact = 38
 
 -- Manage & create your pawn shops here
 Config.Shops = {
-    ['vinewood'] = { -- Unique identifier for this shop
-        name = 'Vinewood Pawn & Jewelry', -- Shop name
+    ['morningwood'] = { -- Unique identifier for this shop
+        name = 'Morningwood Pawn & Jewelry', -- Shop name
         slots = 25, -- How many slots are available
         weight = 100000, -- How much weight is available
-        coords = vec4(-1459.2361, -413.2576, 36.2567, 343.8426), -- Where this shop exists
+        coords = vec4(-1459.4126, -413.5105, 35.7555, 161.3566), -- Where this shop exists
         radius = 1.0, -- How large of a circle zone radius (for targeting only)
         spawnPed = false, -- Spawn a ped to interact with here?
         pedModel = 'a_m_y_beach_02', -- If spawnPed = true, what ped model?
@@ -41,8 +41,8 @@ Config.Shops = {
         -- Min is the earliest the shop is available (default 06:00AM)
         -- Max is the latest the shop is available (detault 21:00 aka 9PM)
         -- If you want it available 24/7, set min to 1 and max to 24
-        hour = { min = 6, max = 21 },
-        account = 'cash', -- Give 'cash', 'bank' or 'dirty' money when selling here?
+        hour = { min = 1, max = 24 },
+        account = 'black_money', -- Give 'cash', 'bank' or 'dirty' money when selling here?
         allowlist = {
             -- What items can be sold here
             -- Any item not in this list, cannot be sold here
@@ -61,35 +61,11 @@ Config.Shops = {
         -- An empty inventory, and the "slots" amount above will not be overridden
         placeholders = true,
         blip = {
-            enabled = true, -- Enable or disable the blip for this shop
+            enabled = false, -- Enable or disable the blip for this shop
             sprite = 59, -- Sprite ID (https://docs.fivem.net/docs/game-references/blips/)
             color = 0, -- Color (https://docs.fivem.net/docs/game-references/blips/#blip-colors)
             scale = 0.8, -- Size/scale
             label = 'Pawn Shop' -- Label
         }
-    },
-    ['strawberry'] = {
-        name = 'Strawberry Ave Pawn Shop',
-        slots = 25,
-        weight = 100000,
-        coords = vec4(182.7942, -1319.3451, 29.3173, 244.3924),
-        radius = 1.0,
-        spawnPed = true,
-        pedModel = 'a_m_y_beach_02',
-        hour = { min = 6, max = 21 },
-        account = 'cash',
-        allowlist = {
-            ['burger'] = { label = 'Burger', price = 25 },
-        },
-        placeholders = false,
-        blip = {
-            enabled = true,
-            sprite = 59,
-            color = 0,
-            scale = 0.8,
-            label = 'Pawn Shop'
-        }
-    },
-    -- Add more pawn shops here as desired
-    -- Be sure to follow the same format as above
+    }
 }
